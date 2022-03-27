e86cd470530c74a292f0b439954fe57106720eae
Siu <siuc@protonmail.com>
2022-03-26 22:53:31 -0400

    add background image

--- src/App.vue
+++ src/App.vue
@@ -7,4 +7,11 @@ export default {};
 </script>
 
 <style>
+html {
+  background: url("./assets/background.jpg") no-repeat center center fixed;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
 </style>
