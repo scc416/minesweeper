9e729ac2aac78a8099b2cb91ff1035bff325c2e1
Siu <siuc@protonmail.com>
2022-03-27 16:43:00 -0400

    add padding for score

--- src/components/Board/Status/Score.vue
+++ src/components/Board/Status/Score.vue
@@ -12,11 +12,13 @@ export default {};
 span {
   background: #000000;
   color: #eb0000;
-  height: 0.7em;
+  height: 0.68em;
+  border-right: #e1dcdc solid 0.05em;
+  border-bottom: #e1dcdc solid 0.05em;
 }
 
 span > * {
-  bottom: 0.15em;
+  bottom: 0.19em;
   position: relative;
 }
 </style>
--- src/components/Board/Status/Status.vue
+++ src/components/Board/Status/Status.vue
@@ -14,14 +14,14 @@ export default { components: { Score } };
 <style scoped>
 div {
   background: #bdbdbd;
-  border-left: #787878 solid 0.12em;
-  border-top: #787878 solid 0.12em;
-  border-bottom: #ffffff solid 0.12em;
-  border-right: #ffffff solid 0.12em;
-  padding: 0.2em 0.2em;
-  margin-bottom: 0.42em;
+  border-left: #787878 solid 0.06em;
+  border-top: #787878 solid 0.06em;
+  border-bottom: #ffffff solid 0.06em;
+  border-right: #ffffff solid 0.06em;
+  padding: 0.15em 0.15em;
+  margin-bottom: 0.16em;
   font-family: "VT323";
-  font-size: 2em;
+  font-size: 2.8em;
   display: flex;
   justify-content: space-between;
 }
