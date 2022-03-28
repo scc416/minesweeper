57ac2f52f5de7a4d3a610c5693b4ec2ccf1e3846
Siu <siuc@protonmail.com>
2022-03-28 11:36:09 -0400

    new constants for state of game

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const initState = {
   startTime: null,
   clicked: [],
   flagged: [],
-  bombs: []
+  bombs: [],
 };
 
 export const numOfBomb = 10;
@@ -10,3 +10,8 @@ export const numOfBomb = 10;
 export const MINE_FILEPATH = "../../../assets/icon/mine.png";
 
 export const numOfRow = 8;
+
+export const GAME_PENDING = "GAME_PENDING";
+export const GAME_ON = "GAME_ON";
+export const GAME_WIN = "GAME_WIN";
+export const GAME_LOSE = "GAME_LOSE";
